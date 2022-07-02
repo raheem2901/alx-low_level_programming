9d7aaa32c956e54a7cf56ca084625e60d9772ae8
Raheem Abdulquadir <raheemquadir2003@gmail.com>
2022-07-02 14:50:47 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * main - prints the single digit numbers
  *  and followed by new line
@@ -9,7 +10,7 @@ int main(void)
 
 	for (n = 0; n < 10; n++)
 	{
-		printf("%d"\n, n);
+		printf("%d\n", n);
 	}
 
 	return (0);
