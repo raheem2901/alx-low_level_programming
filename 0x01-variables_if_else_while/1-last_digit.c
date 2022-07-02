590178998ae3b49f3f8019e9f4b2be8b5e43f0a0
Raheem Abdulquadir <raheemquadir2003@gmail.com>
2022-07-02 03:48:40 -0700

    This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,9 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 
-/* 
+/*
  * main -> assign a random number to the variable n each time it is executed
- * and print the last digit of the number stored in the variable 
+ * and print the last digit of the number stored in the variable
  * Return : always 0
   */
 int main(void)
@@ -13,17 +13,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("last digit of %d is ",n);
-	if( n > 5)
+	printf("last digit of %d is ", n);
+	if (n > 5)
 	{
 		printf("greater than 5");
 
 	}
-	if ( n == 0)
+	if (n == 0)
 	{
 		printf("0";)
 	}
-	if (n < 6 && n !=0)
+	if (n < 6 && n != 0)
 	{
 		printf("less than 6 not 0");
 	}
