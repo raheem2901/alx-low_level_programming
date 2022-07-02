67c97f37a1bf54b7be887c1a02805f953b71836a
Raheem Abdulquadir <raheemquadir2003@gmail.com>
2022-07-02 16:10:24 -0700

    Task 6 done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints the single digit numbers
+ *  and followed by new line
+ *  You are not allowed to use any variable of type char
+ *  Return: always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch < 57; ch++)
+	{
+	putchar(ch);
+	}
+	putchar(10);
+	return (0);
+
+}
+
