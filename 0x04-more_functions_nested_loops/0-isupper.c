e341720189b8c91ca8d9901e1798e489c6354178
Raheem Abdulquadir <raheemquadir2003@gmail.com>
2022-07-07 08:26:13 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check the alphabets for upper case
+ *@c: and integer paramer
+ * Return: something but success
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
