7c32ec0d9e3d20eafdeea4810fb7d08f2b36434c
Raheem Abdulquadir <raheemquadir2003@gmail.com>
2022-07-06 19:07:04 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _islower - check if the chaacter is lower
+ *  Return: return 1 and 0 depending on condition
+ */
+
+int _islower(int c)
+
+{
+	return(c >= 'a' && c <= 'z');
+}
