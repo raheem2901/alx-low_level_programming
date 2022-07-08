3e49f942ef394ccc0086fb75105cc67abfe77c79
Raheem Abdulquadir <raheemquadir2003@gmail.com>
2022-07-07 17:36:43 -0700

    fixing 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,23 +2,20 @@
 
 /**
  * more_numbers - prints 10 times the numbers
- *
- * Return: always 0
  */
-
 void more_numbers(void)
 {
 	int x, y;
 
 	for (x = 0; x < 10; x++)
 	{
-		for (y = 0; y <= 15; y++)
+		for (y = 0; y < 15; y++)
 		{
 			if (y >= 10)
 				_putchar((y / 10) + '0');
 			_putchar((y % 10) + '0');
 		}
-		_putchar("\n");
+		_putchar('\n');
 	}
 }
 
