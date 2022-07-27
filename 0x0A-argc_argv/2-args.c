d32d61299c7d03702d174a2fc42f85d1a9c043cd
Raheem Abdulquadir <raheemquadir2003@gmail.com>
2022-07-27 14:54:18 -0700

    print all arguments one per line include first one

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,14 +6,14 @@
  * @argv: an array of a command listed
  * Return: 0 for success
  */
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("argv index %d is = %s\n", i, argv[i]);
+		printf("%s\n", argv[i]);
 	}
-	return(0);
+	return (0);
 }
 
