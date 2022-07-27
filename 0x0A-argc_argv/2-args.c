140cea2cf8ab183157b07f46d201983e18d32dd4
Raheem Abdulquadir <raheemquadir2003@gmail.com>
2022-07-27 14:50:07 -0700

    print all arguments one per line include first one

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this is a function to print its name
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("argv index %d is = %s\n", i, argv[i]);
+	}
+	return(0);
+}
+
